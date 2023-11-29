8284e4c91f45bddd541fc789570aed0faf100c41
mlaskowski7 <144243838+mlaskowski7@users.noreply.github.com>
2023-11-29 22:25:34 +0100

    adding the loader

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = false;
+}
+
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 function newQuote(){
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
     if(!quote.author){
@@ -23,9 +33,11 @@ function newQuote(){
         quoteText.classList.remove('long-quote');
     }
     quoteText.textContent = quote.text;
+    complete();
 }
 
 async function getQuotes(){
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try{
         const response = await fetch(apiUrl);
